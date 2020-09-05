d0b08335f45e121adb26518251eb0c2642d9bff5
Kinjal Raykarmakar <kinjalrk2k@gmail.com>
2020-09-06 01:58:08 +0530

    Initialized landing page

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs")
+
+app.get("/", function(req, res) {
+    res.render("landing")
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function () {
   console.log(`The YelpCamp Server is running on port ${PORT}`);
